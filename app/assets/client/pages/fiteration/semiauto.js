280747ee46465ec7379e0856df47c7c25d171b5d
guo <1599485508@qq.com>
2016-02-13 16:20:19 +0800

    save

---
+++ app/assets/client/pages/fiteration/semiauto.js
@@ -0,0 +1,12 @@
+//
+// =require components/common/index
+// =require components/page-header/index
+//
+//=require_self
+
+$(function(){
+	var el=$('body');
+
+	el.find('.com-page-header').PageHeader();
+
+})
