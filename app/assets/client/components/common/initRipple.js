c92d0f9530ff0d7a388875a1ba9d519a6f6d59fb
guo <1599485508@qq.com>
2016-03-20 19:21:59 +0800

    save

--- app/assets/client/components/common/initRipple.js
+++ app/assets/client/components/common/initRipple.js
@@ -1,39 +1,59 @@
 //
 //
-//=require_self
-//
-;
-(function($,window,document,undefined){
-	function initRipple(){
-		var el=$('body'),
-			x,y,size;
-		el.on('animationend webkitAnimationEnd','.ripple .ink',function(e){
-			e.preventDefault();
-			$(this).remove();
-		});
-		el.on("click",".ripple",function(e){
-			e.preventDefault();
-			var elCurrent=$(e.currentTarget);
-			if(elCurrent.find('.ink').length==0){
-				elCurrent.prepend('<span class="ink"></span>');
-			}
-			var elInk=el.find('.ink');
-			elInk.removeClass('animate');
-			if(!elInk.height()&&!elInk.width()){
-				size=Math.max(elCurrent.outerHeight(),elCurrent.outerWidth());
-				elInk.css({
-					height:size,
-					width:size
-				});
-			}
-			x=e.pageX-elCurrent.offset().left-elInk.width()/2;
-			y=e.pageY-elCurrent.offset().top-elInk.height()/2;
-			elInk.css({
-				left:x+"px",
-				top:y+"px"
-			}).addClass('animate');
+//= require_self
 
-		});
-	}
-	$.fn.initRipple=initRipple;
-})(window.jQuery||window.Zepto,window,document);
+
+;
+(function($, window, document, undefined) {
+    function initRipple() {
+        var el = $('body'),
+            x, y, size;
+        //     isSupportCSSAnimations = $('html').hasClass('cssanimations');
+
+        // if (!isSupportCSSAnimations) {
+        //     return;
+        // }
+
+        el.on('animationend webkitanimationend mozanimationend msanimationend oanimationend', '.ripple .ink', function(e) {
+            e.preventDefault();
+
+            var elCurrentTarget = $(e.currentTarget);
+
+            elCurrentTarget.remove();
+        });
+
+        el.on('click', '.ripple', function(e) {
+            e.preventDefault();
+
+            var elCurrentTarget = $(e.currentTarget);
+
+            if (elCurrentTarget.find(".ink").length == 0) {
+                elCurrentTarget.prepend("<span class='ink'></span>");
+            }
+
+            elInk = elCurrentTarget.find(".ink");
+            //incase of quick double clicks stop the previous animation
+            elInk.removeClass("animate");
+
+            //set size of .ink
+            if (!elInk.height() && !elInk.width()) {
+                size = Math.max(elCurrentTarget.outerWidth(), elCurrentTarget.outerHeight());
+                elInk.css({
+                    height: size,
+                    width: size
+                });
+            }
+
+            x = e.pageX - elCurrentTarget.offset().left - elInk.width() / 2;
+            y = e.pageY - elCurrentTarget.offset().top - elInk.height() / 2;
+
+
+            elInk.css({
+                top: y + 'px',
+                left: x + 'px'
+            }).addClass("animate");
+        });
+    }
+
+    $.fn.initRipple = initRipple;
+})(window.jQuery || window.Zepto, window, document);
