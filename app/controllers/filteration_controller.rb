fa0b2b9c68b7f4106defab7b64bc4fdc9bcb830a
luohao <425778179@qq.com>
2016-05-22 21:22:47 +0800

    save

--- app/controllers/filteration_controller.rb
+++ app/controllers/filteration_controller.rb
@@ -13,4 +13,48 @@ class FilterationController < BaseController
 	
 	def detail
 	end
+
+	def download_helix_200
+    		send_file("files/20160505_AZUD_HELIX_AUTO_200300 ENG.pdf" , disposition: :attachment)
+  	end
+
+  	def download_helix_201
+    		send_file("files/20160505_AZUD_HELIX_AUTO_201 ENG.pdf" , disposition: :attachment)
+  	end
+
+    	def download_helix_dcl
+    		send_file("files/20160505_AZUD_HELIX_AUTO_4DCL_4DCH ENG.pdf" , disposition: :attachment)
+  	end
+
+  	def download_helix_400
+    		send_file("files/20160505_AZUD_HELIX_AUTO_4DCL_4DCH ENG.pdf" , disposition: :attachment)
+  	end
+
+  	def download_luxon_lca
+  		send_file("files/20160505_AZUD_LUXON_LCA ENG.pdf" , disposition: :attachment)
+  	end
+
+  	def download_luxon_mfh
+  		send_file("files/20160505_AZUD_LUXON_MFH ENG.pdf" , disposition: :attachment)
+  	end
+
+  	def download_luxon_ldb
+  		send_file("files/20160505_AZUD_LUXON_LDB ENG.pdf" , disposition: :attachment)
+  	end
+
+  	def download_luxon_lxe
+  		send_file("files/20160505_AZUD_LUXON_LXE ENG.pdf" , disposition: :attachment)
+  	end
+
+  	def download_luxon_lem
+  		send_file("files/20160505_AZUD_LUXON_LEM ENG.pdf" , disposition: :attachment)
+  	end
+
+  	def download_luxon_lkm#???
+  		send_file("files/20160505_AZUD_LUXON_LEM ENG.pdf" , disposition: :attachment)
+  	end
+  	
+  	def download_luxon_lpf
+  		send_file("files/20160505_AZUD_LUXON_LPF ENG.pdf" , disposition: :attachment)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,17 @@ Azud::Application.routes.draw do
       get "/luxon" => "filteration#luxon"
       get "/semiauto" => "filteration#semiauto"
       get "/helix/detail" => "filteration#detail"
+      get "/download_helix_200" => "filteration#download_helix_200"
+      get "/download_helix_201" => "filteration#download_helix_201"
+      get "/download_helix_dcl" => "filteration#download_helix_dcl"
+      get "/download_helix_400" => "filteration#download_helix_400"
+      get "/download_luxon_lca" => "filteration#download_luxon_lca"
+      get "/download_luxon_mfh" => "filteration#download_luxon_mfh"
+      get "/download_luxon_ldb" => "filteration#download_luxon_ldb"
+      get "/download_luxon_lxe" => "filteration#download_luxon_lxe"
+      get "/download_luxon_lem" => "filteration#download_luxon_lem"
+      get "/download_luxon_lkm" => "filteration#download_luxon_lkm"
+      get "/download_luxon_lpf" => "filteration#download_luxon_lpf"
     end
   end
 
