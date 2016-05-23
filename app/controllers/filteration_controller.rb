c9efeeec43a1cf718e021d4d6e8ea60dd5b46ae1
guo <1599485508@qq.com>
2016-05-23 17:02:58 +0800

    下载改为浏览

--- app/controllers/filteration_controller.rb
+++ app/controllers/filteration_controller.rb
@@ -15,70 +15,70 @@ class FilterationController < BaseController
 	end
 
 	def download_helix_200
-    		send_file("files/20160505_AZUD_HELIX_AUTO_200300 ENG.pdf" , disposition: :attachment)
+    		send_file("files/20160505_AZUD_HELIX_AUTO_200300 ENG.pdf" , disposition: :inline)
   	end
 
   	def download_helix_201
-    		send_file("files/20160505_AZUD_HELIX_AUTO_201 ENG.pdf" , disposition: :attachment)
+    		send_file("files/20160505_AZUD_HELIX_AUTO_201 ENG.pdf" , disposition: :inline)
   	end
 
     	def download_helix_dcl
-    		send_file("files/20160505_AZUD_HELIX_AUTO_4DCL_4DCH ENG.pdf" , disposition: :attachment)
+    		send_file("files/20160505_AZUD_HELIX_AUTO_4DCL_4DCH ENG.pdf" , disposition: :inline)
   	end
 
   	def download_helix_400
-    		send_file("files/20160505_AZUD_HELIX_AUTO_4DCL_4DCH ENG.pdf" , disposition: :attachment)
+    		send_file("files/20160505_AZUD_HELIX_AUTO_4DCL_4DCH ENG.pdf" , disposition: :inline)
   	end
 
   	def download_luxon_lca
-  		send_file("files/20160505_AZUD_LUXON_LCA ENG.pdf" , disposition: :attachment)
+  		send_file("files/20160505_AZUD_LUXON_LCA ENG.pdf" , disposition: :inline)
   	end
 
   	def download_luxon_mfh
-  		send_file("files/20160505_AZUD_LUXON_MFH ENG.pdf" , disposition: :attachment)
+  		send_file("files/20160505_AZUD_LUXON_MFH ENG.pdf" , disposition: :inline)
   	end
 
   	def download_luxon_ldb
-  		send_file("files/20160505_AZUD_LUXON_LDB ENG.pdf" , disposition: :attachment)
+  		send_file("files/20160505_AZUD_LUXON_LDB ENG.pdf" , disposition: :inline)
   	end
 
   	def download_luxon_lxe
-  		send_file("files/20160505_AZUD_LUXON_LXE ENG.pdf" , disposition: :attachment)
+  		send_file("files/20160505_AZUD_LUXON_LXE ENG.pdf" , disposition: :inline)
   	end
 
   	def download_luxon_lem
-  		send_file("files/20160505_AZUD_LUXON_LEM ENG.pdf" , disposition: :attachment)
+  		send_file("files/20160505_AZUD_LUXON_LEM ENG.pdf" , disposition: :inline)
   	end
 
   	def download_luxon_lkm#???
-  		send_file("files/20160505_AZUD_LUXON_LEM ENG.pdf" , disposition: :attachment)
+  		send_file("files/20160505_AZUD_LUXON_LEM ENG.pdf" , disposition: :inline)
   	end
   	
   	def download_luxon_lpf
-  		send_file("files/20160505_AZUD_LUXON_LPF ENG.pdf" , disposition: :attachment)
+  		send_file("files/20160505_AZUD_LUXON_LPF ENG.pdf" , disposition: :inline)
   	end
 
     def download_semi_lcm
-      send_file("files/20160505_AZUD_SIMI_SYSTEM_LCM ENG.pdf" , disposition: :attachment)
+      send_file("files/20160505_AZUD_SIMI_SYSTEM_LCM ENG.pdf" , disposition: :inline)
     end
 
     def download_semi_spiral
-      send_file("files/20160505_AZUD_SPIRAL_CLEAN ENG.pdf" , disposition: :attachment)
+      send_file("files/20160505_AZUD_SPIRAL_CLEAN ENG.pdf" , disposition: :inline)
     end
 
     def download_semi_system
-      send_file("files/20160505_AZUD_SEMI_SYSTEM ENG.pdf" , disposition: :attachment)
+      send_file("files/20160505_AZUD_SEMI_SYSTEM ENG.pdf" , disposition: :inline)
     end
 
     def download_semi_100
-      send_file("files/20160505_AZUD_SEMI_MODULAR_100 ENG.pdf" , disposition: :attachment)
+      send_file("files/20160505_AZUD_SEMI_MODULAR_100 ENG.pdf" , disposition: :inline)
     end
 
     def download_semi_300
-      send_file("files/20160505_AZUD_SEMI_MODULAR_300 ENG.pdf" , disposition: :attachment)
+      send_file("files/20160505_AZUD_SEMI_MODULAR_300 ENG.pdf" , disposition: :inline)
     end
 
     def download_semi_agl
-      send_file("files/20160505_AZUD_SEMI_AGL ENG.pdf" , disposition: :attachment)
+      send_file("files/20160505_AZUD_SEMI_AGL ENG.pdf" , disposition: :inline)
     end
 end
