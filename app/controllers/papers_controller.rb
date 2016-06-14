e67cc1e33e24f62343fa6f970434461c26d6fbdc
luohao <425778179@qq.com>
2016-06-15 00:03:36 +0800

    paper修改

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,8 +1,9 @@
 # coding: utf-8
 class PapersController < BaseController
   def index
+    @front_papers = Paper.where(["front = ?", true]).order("updated_at DESC").limit(3)
     page = params[:page] || 1
-    @papers = Paper.where("state = true").paginate(:page => page, :per_page => 5).order("id DESC")
+    @papers = Paper.paginate(:page => page, :per_page => 5).order("id DESC")
     respond_to do |format|
       format.html
       format.json{ render json: @papers }
@@ -11,6 +12,16 @@ class PapersController < BaseController
 
   def show
     @paper = Paper.find(params[:id])
+    @count = Paper.count
+    @last_paper_id = Paper.last.id
+    @first_paper_id = Paper.first.id
+    @id_array = if @paper.id == @last_paper_id
+      [Paper.where(["id < ?", @paper.id]).last.id , nil]
+    elsif @paper.id == @first_paper_id
+      [nil , @next_paper_id = Paper.where(["id > ?", @paper.id]).first.id]
+    else
+      [Paper.where(["id < ?", @paper.id]).last.id , Paper.where(["id > ?", @paper.id]).first.id]
+    end
     @content = change_img_root(@paper.content)
   end
 
