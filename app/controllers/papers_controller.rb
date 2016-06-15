24a19954d0a22c8f7ad028a020ebfa1280a80ef8
luohao <425778179@qq.com>
2016-06-16 00:13:18 +0800

    save

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -15,12 +15,14 @@ class PapersController < BaseController
     @count = Paper.count
     @last_paper_id = Paper.last.id
     @first_paper_id = Paper.first.id
-    @id_array = if @paper.id == @last_paper_id
-      [Paper.where(["id < ?", @paper.id]).last.id , nil]
-    elsif @paper.id == @first_paper_id
-      [nil , @next_paper_id = Paper.where(["id > ?", @paper.id]).first.id]
-    else
-      [Paper.where(["id < ?", @paper.id]).last.id , Paper.where(["id > ?", @paper.id]).first.id]
+    if @count != 1
+      @id_array = if @paper.id == @last_paper_id
+        [Paper.where(["id < ?", @paper.id]).last.id , nil]
+      elsif @paper.id == @first_paper_id
+        [nil , Paper.where(["id > ?", @paper.id]).first.id]
+      else
+        [Paper.where(["id < ?", @paper.id]).last.id , Paper.where(["id > ?", @paper.id]).first.id]
+      end
     end
     @content = change_img_root(@paper.content)
   end
